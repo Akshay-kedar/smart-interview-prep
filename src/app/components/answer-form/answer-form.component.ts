b601e85297b46f75fd0e87c5294fecc7f5e5ef34
Akshay Kedar <akshay.kedar@das.cpa.com>
2025-04-05 13:30:36 +0530

    creating required component structure

---
+++ src/app/components/answer-form/answer-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-answer-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './answer-form.component.html',
+  styleUrl: './answer-form.component.css'
+})
+export class AnswerFormComponent {
+
+}
---
+++ src/app/components/job-selector/job-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-selector.component.html',
+  styleUrl: './job-selector.component.css'
+})
+export class JobSelectorComponent {
+
+}
